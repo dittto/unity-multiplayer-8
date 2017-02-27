ca2c6802c4e4479fa31d1ed6a53fcc4b9fcef27c
Nick Stacey <nick@ditt.to>
2017-02-27 23:48:06 +0000

    Initial import

---
+++ Assets/Level/LevelStart.cs
@@ -0,0 +1,15 @@
+﻿using GameState;
+using UnityEngine;
+
+namespace Assets.Level {
+    class LevelStart:MonoBehaviour {
+
+        public void Start()
+        {
+            Debug.Log("Level Start");
+            State.GetInstance()
+                .Level(State.LEVEL_READY)
+                .Publish();
+        }
+    }
+}
